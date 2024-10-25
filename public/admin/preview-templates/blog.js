a4c0f63af5f5fbad32b8e09e0796491c14ca2b82
jaybird101 <me@jasonadle.com>
2024-10-25 01:36:26 -0400

    modify decap previews

--- public/admin/preview-templates/blog.js
+++ public/admin/preview-templates/blog.js
@@ -13,35 +13,6 @@ const Blog = createClass({
         ${this.props.widgetFor('body')}
         <hr/>
 
-        <!-- Article List Placeholder (for illustrative purposes) -->
-        <section>
-          <article class="article-card article-card--tags">
-            <a class="article-card__link" href="#">
-              <h5 class="article-card__title">Placeholder Article</h5>
-              <p class="article-card__summary">
-                This is not a real article and is only visible in the CMS.
-              </p>
-              <p class="tag-list">
-                <a class="tag" href="#" rel="tag">Politics</a>
-                <a class="tag" href="#" rel="tag">Philosophy</a>
-                <a class="tag" href="#" rel="tag">Technology</a>
-              </p>
-            </a>
-          </article>
-          <article class="article-card article-card--tags">
-            <a class="article-card__link" href="#">
-              <h5 class="article-card__title">Placeholder Article</h5>
-              <p class="article-card__summary">
-                This is not a real article and is only visible in the CMS.
-              </p>
-              <p class="tag-list">
-                <a class="tag" href="#" rel="tag">Politics</a>
-                <a class="tag" href="#" rel="tag">Philosophy</a>
-                <a class="tag" href="#" rel="tag">Technology</a>
-              </p>
-            </a>
-          </article>
-        </section>
       </main>
     `;
   },
